1df33f0828b421c10aec35b99543e2307e243e15
wongluyee <wongluyee1015@hotmail.com>
2023-09-06 09:45:11 +0900

    create model

--- WEB-INF/src/bms/Book.java
+++ WEB-INF/src/bms/Book.java
@@ -1,5 +1,32 @@
 package bms;
 
 public class Book {
+	// フィールド変数
+	private String isbn;
+	private String title;
+	private int price;
 
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
 }
--- WEB-INF/src/bms/BookDAO.java
+++ WEB-INF/src/bms/BookDAO.java
@@ -1,5 +1,28 @@
 package bms;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class BookDAO {
 
+	// データベース接続情報
+	private static String RDB_DRIVE = "com.mysql.jdbc.Driver";
+	private static String URL = "jdbc:mysql://localhost/mybookdb";
+	private static String USER = "root";
+	private static String PASS = "root123";
+
+	private static Connection getConnection() {
+		try {
+			// JDBCドライバーを読み込む
+			Class.forName(RDB_DRIVE);
+
+			// Connectionする
+			Connection con = DriverManager.getConnection(URL, USER, PASS);
+
+			return con;
+
+		} catch (Exception e) {
+			throw new IllegalStateException(e);
+		}
+	}
 }
